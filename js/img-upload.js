4a3e095086ba8cf721430cb7398a55a9c04a4ce8
Darknessich <blakibrown@gmail.com>
2024-03-16 14:36:55 +0300

    add validator

--- js/img-upload.js
+++ js/img-upload.js
@@ -1,27 +1,17 @@
 import { isEscapeKey } from './util.js';
+import { addFormValidator } from './upload-form-validate.js';
 
 const uploadForm = document.querySelector('#upload-select-image');
 const uploadFile = uploadForm.querySelector('#upload-file');
 const uploadModal = uploadForm.querySelector('.img-upload__overlay');
 const uploadClose = uploadForm.querySelector('.img-upload__cancel');
-
-let isFocus = false;
-
-function onUploadFormFocus(evt) {
-  isFocus = ![uploadForm, uploadClose, uploadFile].includes(evt.target);
-}
-
-function onUploadFormBlur() {
-  isFocus = false;
-}
+const uploadTags = uploadForm.querySelector('.text__hashtags');
+const uploadComment = uploadForm.querySelector('.text__description');
 
 function openUploadModal() {
   document.body.classList.add('modal-open');
   uploadModal.classList.remove('hidden');
 
-  isFocus = false;
-  uploadForm.addEventListener('focus', onUploadFormFocus, true);
-  uploadForm.addEventListener('blur', onUploadFormBlur, true);
   document.addEventListener('keydown', onDocumentKeyDown);
 }
 
@@ -34,13 +24,11 @@ function closeUploadModal() {
   uploadModal.classList.add('hidden');
   uploadForm.reset();
 
-  uploadForm.removeEventListener('focus', onUploadFormFocus, true);
-  uploadForm.removeEventListener('blur', onUploadFormBlur, true);
   document.removeEventListener('keydown', onDocumentKeyDown);
 }
 
 function onDocumentKeyDown(evt) {
-  if (isEscapeKey(evt) && !isFocus) {
+  if (isEscapeKey(evt)) {
     evt.preventDefault();
     closeUploadModal();
   }
@@ -50,5 +38,14 @@ function onUploadCloseClick() {
   closeUploadModal();
 }
 
+function onInputKeyDown(evt) {
+  if (isEscapeKey(evt)) {
+    evt.stopPropagation();
+  }
+}
+
+addFormValidator(uploadForm, uploadTags, uploadComment);
 uploadFile.addEventListener('change', onUploadFileChange);
 uploadClose.addEventListener('click', onUploadCloseClick);
+uploadTags.addEventListener('keydown', onInputKeyDown);
+uploadComment.addEventListener('keydown', onInputKeyDown);
--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
 import { createPicturesData } from './data.js';
 import { renderGallery } from './gallery.js';
 import './img-upload.js';
-import './upload-form-validate.js';
 
 const pictures = createPicturesData();
 renderGallery(pictures);
